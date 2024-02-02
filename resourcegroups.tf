9d4ff0115734b61923d8ad8c80816831da2c1a48
Stom1294 <snehamerlin7@gmail.com>
2024-02-02 14:43:28 -0500

    Adding resource groups

--- resourcegroups.tf
+++ resourcegroups.tf
@@ -7,3 +7,11 @@ resource "azurerm_resource_group" "ter-rg02" {
   name     = "TerraformTest02"
   location = "West Central US"
 }
+resource "azurerm_resource_group" "azure-dev-rg01-westcentral" {
+  name = "terraform-wcus"
+  location = "West Central US"
+}
+resource "azurerm_resource_group" "azure-dev-rg02-eastUS2" {
+  name = "terraform-eus2"
+  location = "East US 2"
+}
